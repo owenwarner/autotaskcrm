f04dd7a831198c73d695c7aec689f72d638cdbc4
owenwarner <owenwarner@gmail.com>
2014-12-23 15:16:22 -0500

    Updating autotaskcrm to support creating tickets and ticket notes

--- lib/autotaskcrm.rb
+++ lib/autotaskcrm.rb
@@ -27,18 +27,38 @@ class AutoTaskCrm
     end
   end
 
-  def send_xml(xml)
-    resp = @client.request :query do
-      soap.body = { :sXML => "<queryxml>#{xml}</queryxml>" } 
+  def send_xml(xml, query = true)
+    if query == true
+      resp = @client.request :query do
+        soap.body = { :sXML => "<queryxml>#{xml}</queryxml>" }
+      end
+      resp.body[:query_response][:query_result][:entity_results].is_a?(Hash) ? resp : false
+    else
+      resp = @client.request :create do
+        full_xml = "<?xml version='1.0' encoding='UTF-8'?><soap:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap='http://www.w3.org/2003/05/soap-envelope'><soap:Body><create xmlns='http://autotask.net/ATWS/v1_5/'>#{xml}</create></soap:Body></soap:Envelope>"
+        soap.xml = full_xml
+      end
     end
+  end
 
-    resp.body[:query_response][:query_result][:entity_results].is_a?(Hash) ? resp : false
+  def create_ticket(account_id, title, description, priority, due_date, contact_id, source)
+    return nil if account_id.blank? or title.blank? or description.blank?
+
+    resp = send_xml("<Entities><Entity xsi:type='Ticket'><AccountID>#{account_id}</AccountID><Priority>#{priority}</Priority><Status>1</Status><Title>#{title}</Title><Description xsi:type='xsd:string'>#{description}</Description><UserDefinedFields /><DueDateTime>#{DateTime.parse(due_date).strftime("%Y-%m-%d %H:%M:%S")}</DueDateTime><ContactID>#{contact_id}</ContactID><QueueID>29730010</QueueID><WorkType>29730009</WorkType><Source>#{source}</Source></Entity></Entities>", query = false)
+    resp != false ? resp.body : nil
+  end
+
+  def create_ticket_note(ticket_id, title, description)
+    return nil if ticket_id.blank? or title.blank? or description.blank?
+
+    resp = send_xml("<Entities><Entity xsi:type='TicketNote'><TicketID>#{ticket_id}</TicketID><Title>#{title}</Title><Description>#{description}</Description><NoteType>18</NoteType><Publish>1</Publish></Entity></Entities>", query = false)
+    resp != false ? resp.body : nil
   end
 
   def get_ticket_id(ticket_name)
       return nil unless ticket_name.match(Regexp.new(/^T[0-9]{8}\.[0-9]{4}$/))
 
-      resp = send_xml("<entity>ticket</entity><query><field>ticketnumber<expression op='equals'>#{ticket_name.strip}</expression></field></query>") 
+      resp = send_xml("<entity>ticket</entity><query><field>ticketnumber<expression op='equals'>#{ticket_name.strip}</expression></field></query>")
       resp != false ? resp.body[:query_response][:query_result][:entity_results][:entity][:id] : nil
   end
 
@@ -49,9 +69,26 @@ class AutoTaskCrm
     resp != false ? resp.body[:query_response][:query_result][:entity_results][:entity] : nil
   end
 
+  def get_ticket_by_id(ticket_id)
+    return nil if ticket_id.blank?
+
+    resp = send_xml("<entity>ticket</entity><query><field>id<expression op='equals'>#{ticket_id.strip}</expression></field></query>")
+    resp != false ? resp.body[:query_response][:query_result][:entity_results][:entity] : nil
+  end
+  
   def get_ticket_notes(ticket_id)
-    resp = send_xml("<entity>ticketnote</entity><query><field>ticketid<expression op='equals'>#{ticket_id}</expression></field></query>")
-    resp != false ? resp.body[:query_response] : nil
+    resp = send_xml("<entity>ticketnote</entity><query><condition><field>ticketid<expression op='equals'>#{ticket_id}</expression></field></condition><condition><field>Publish<expression op='equals'>1</expression></field></condition><condition><field>notetype<expression op='notequal'>13</expression></field></condition></query>")
+    resp != false ? resp.body[:query_response][:query_result][:entity_results][:entity] : nil
+  end
+
+  def get_ticket_note(ticket_note_id)
+    resp = send_xml("<entity>ticketnote</entity><query><condition><field>id<expression op='equals'>#{ticket_note_id}</expression></field></condition></query>")
+    resp != false ? resp.body[:query_response][:query_result][:entity_results][:entity] : ni
+  end
+  
+  def get_ticket_time_entries(ticket_id)
+    resp = send_xml("<entity>timeentry</entity><query><condition><field>ticketid<expression op='equals'>#{ticket_id}</expression></field></condition><condition><field>type<expression op='equals'>2</expression></field></condition></query>")
+    resp != false ? resp.body[:query_response][:query_result][:entity_results][:entity] : nil
   end
 
   def get_tickets(account_id, year, month)
@@ -106,6 +143,34 @@ class AutoTaskCrm
         </condition>
         <condition operator='OR'>
           <field>QueueID<expression op='equals'>29789587</expression></field>
+        </condition>
+      </condition>
+    </query>
+    EOS
+
+    response = send_xml(query)
+    response != false ? response.body[:query_response][:query_result][:entity_results][:entity] : nil
+  end
+  
+  def get_all_nonrecurring_tickets(account_id)
+    query = <<-EOS
+    <entity>Ticket</entity>
+    <query>
+      <condition>
+        <field>AccountID<expression op='equals'>#{account_id}</expression></field>
+      </condition>
+      <condition>
+        <field>CreateDate<expression op='GreaterThanOrEquals'>#{Date.today - 6.months}</expression></field>
+      </condition>
+      <condition>
+        <condition>
+          <field>QueueID<expression op='NotEqual'>29746061</expression></field>
+        </condition>
+        <condition operator='AND'>
+          <field>QueueID<expression op='NotEqual'>29746065</expression></field>
+        </condition>
+        <condition operator='AND'>
+          <field>QueueID<expression op='NotEqual'>29747641</expression></field>
         </condition>
       </condition>
     </query>
@@ -206,6 +271,13 @@ class AutoTaskCrm
   def get_contact(contact_id)
     Rails.cache.fetch("contact_name_#{contact_id}", :expires_in => 1.weeks) do
       resp = send_xml("<entity>contact</entity><query><field>id<expression op='equals'>#{contact_id}</expression></field></query>")
+      resp != false ? resp.body[:query_response][:query_result][:entity_results][:entity] : nil
+    end
+  end
+  
+  def get_resource(resource_id)
+    Rails.cache.fetch("resource_name_#{resource_id}", :expires_in => 1.weeks) do
+      resp = send_xml("<entity>resource</entity><query><field>id<expression op='equals'>#{resource_id}</expression></field></query>")
       resp != false ? resp.body[:query_response][:query_result][:entity_results][:entity] : nil
     end
   end
