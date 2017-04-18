75d9b87d6477149f56a7f9f7a834681fe4f8d8d2
owenwarner <owenwarner@gmail.com>
2017-04-18 14:23:27 -0400

    Update autotaskcrm.rb

--- lib/autotaskcrm.rb
+++ lib/autotaskcrm.rb
@@ -30,7 +30,7 @@ class AutoTaskCrm
 
   def send_xml(xml, query = true)
     if query == true
-      resp = @client.call(:query, xml: "<queryxml>#{xml}</queryxml>")
+      resp = @client.call(:query, message: "<queryxml>#{xml}</queryxml>")
       resp.body[:query_response][:query_result][:entity_results].is_a?(Hash) ? resp : false
     else
       resp = @client.call(:create, xml: "<?xml version='1.0' encoding='UTF-8'?><soap:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap='http://www.w3.org/2003/05/soap-envelope'><soap:Body><create xmlns='http://autotask.net/ATWS/v1_5/'>#{xml}</create></soap:Body></soap:Envelope>")
